8f4eb71d36c28cbc9e5ab2209a2419972b2d2cf6
Lester Li <Lester.shiyi.li@gmail.com>
2018-05-30 03:22:49 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            <a href="#" target="_blank">练习</a><span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right">
+          <a href="mailto:lester.shiyi.li@gmail.com">联系我们</a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'LaraBBS2') - Laravel 进阶教程 2</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+    </div>
+    @include('layouts._footer')
+  </div>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
